81101dec8170f832af7efec72c0730e323702424
armink <armink.ztl@gmail.com>
2017-10-09 18:37:34 +0800

    1、【修复】注释单词书写错误。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- sfud/src/sfud.c
+++ sfud/src/sfud.c
@@ -251,7 +251,7 @@ static sfud_err hardware_init(sfud_flash *flash) {
         return result;
     }
 
-    /* I found when the flash read mode is supported AAI mode. The flash all blocks is protected,
+    /* I found when the flash write mode is supported AAI mode. The flash all blocks is protected,
      * so need change the flash status to unprotected before write and erase operate. */
     if (flash->chip.write_mode & SFUD_WM_AAI) {
         result = sfud_write_status(flash, true, 0x00);
